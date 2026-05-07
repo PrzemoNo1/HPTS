8993e5b22a197e4f0b4806e49f82cb0d6901c18e
PrzemoNo1 <37351230+PrzemoNo1@users.noreply.github.com>
2026-05-07 12:12:13 +0200

    Simple example with future and promise

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,7 @@
 
 #include <chrono>
 #include <thread>
+#include <future>
 
 std::chrono::milliseconds ONE_SEC(1000);
 std::chrono::milliseconds TWO_SEC(2000);
@@ -11,17 +12,21 @@ std::chrono::milliseconds THREE_SEC(3000);
 
 int main(int argc, char* argv[])
 {
-    //std::this_thread::sleep_for(timespan);
     core::ThreadPool tp(2);
-    tp.submit([](){
+    std::promise<int> p;
+    std::future<int> fu = p.get_future();
+    tp.submit([&p](){
         std::cout << "Starting task A submitted from main" << std::endl;
         std::this_thread::sleep_for(THREE_SEC);
         std::cout << "Finished task A" << std::endl;
+        p.set_value(5);
     });
     tp.submit([](){ std::cout << "Task B submitted from main" << std::endl; });
     //tp.submit([](){ std::cout << "Task C submitted from main" << std::endl; });
 
     std::this_thread::sleep_for(std::chrono::seconds(12));
+
+    std::cout << "Future is: " << fu.get() << std::endl;
 
     return 0;
 }
